b1b781cf339dd40a7a45c9b520e2463f6ebcfa1d
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2020-12-15 21:13:47 -0500

    custom query

--- mark_space_backend/mark_space_api/views.py
+++ mark_space_backend/mark_space_api/views.py
@@ -7,17 +7,18 @@ from .models import Teacher, Student
 
 class TeacherView(viewsets.ModelViewSet):
     serializer_class = TeacherSerializer
-    queryset = Teacher.objects.none()
+    queryset = Teacher.objects.all()
 
     def list(self, request, *args, **kwargs):
-        queryset = Teacher.objects.all()
+        queryset = self.get_queryset()
         serializer = TeacherSerializer(queryset, many=True)
         return Response(serializer.data)
 
-    def retrieve(self, request, *args, **kwargs):
-        email = self.request.query_params.get('email')
-        queryset = Teacher.objects.filter(name__contains=email)
-        return queryset
+    def get_queryset(self):
+        queryset = self.queryset
+        print(self.request.query_params)
+        query_set = queryset.filter(email=self.request.query_params.get('email'))
+        return query_set
 
 
 class StudentView(viewsets.ModelViewSet):
